dd64e3204528f0c5af03571aa9d7860076ba096f
Franz Gollhammer <franz.gollhammer@gmail.com>
2018-03-23 18:59:17 +0100

    Added firebase api

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
+
+class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyAzCxApRDoYOLG-qYZ_0_h0dBVWzLe24jc",
+      authDomain: "authentication-6e1b7.firebaseapp.com",
+      databaseURL: "https://authentication-6e1b7.firebaseio.com",
+      projectId: "authentication-6e1b7",
+      storageBucket: "authentication-6e1b7.appspot.com",
+      messagingSenderId: "400805764706"
+  });
+  }
+
+  render() {
+    return (
+      <View>
+        <Header headerText="Authentication" />
+        <Text>An App</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
