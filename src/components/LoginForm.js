0de106393499d1748f910df7cacea196c2fdd752
Franz Gollhammer <franz.gollhammer@gmail.com>
2018-03-23 21:53:26 +0100

    Added password form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react'
-import { Button, Card, CardSection, Input } from './common'
+import firebase from 'firebase';
+import { Button, Card, CardSection, Input, secureTextEntry } from './common'
 
 class LoginForm extends Component {
-  state = { text: '' }
+  state = { email: '', password: '' }
+
+  onButtonPress() {
+    const { email, password } = this.state;
+    firebase.auth().signInWithEmailAndPassword(email, password);
+  }
 
   render() {
     return(
@@ -11,15 +17,23 @@ class LoginForm extends Component {
           <Input
             placeholder="user@example.com"
             label="Email"
-            value={this.state.text}
-            onChangeText={text => this.setState({ text })}
+            value={this.state.email}
+            onChangeText={email => this.setState({ email })}
           />
         </CardSection>
 
-        <CardSection />
+        <CardSection>
+          <Input
+            secureTextEntry={true}
+            placeholder="password"
+            label="Password"
+            value={this.state.password}
+            onChangeText={password => this.setState({ password })}
+          />
+        </CardSection>
 
         <CardSection>
-          <Button>
+          <Button onPress={this.onButtonPress.bind(this)}>
             Log in  
           </Button>
         </CardSection>
